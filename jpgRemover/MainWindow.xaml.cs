347cb1e10e3f25a6ecbcd1ae00384974f3d4c8e9
mfp_h <mfp_h@HELLFIRE6700K>
2017-03-18 10:00:39 -0700

    Add project files.

---
+++ jpgRemover/MainWindow.xaml.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace jpgRemover
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        List<string> jpgsToRemove = new List<string>();
+        string Output = "";
+
+        int jpegsToRemoveCount = 0;
+
+        public Dictionary<string, string> AppKeys
+        {
+            get
+            {
+                Dictionary<string, string> Keys = new Dictionary<string, string>();
+                Keys.Add("CR2", "Canon RAW Picture files (.CR2)");
+                Keys.Add("DNG", "Nikon RAW Picture files (.DNG)");
+                return Keys;
+            }
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            InitializeSytem();         
+        }
+
+        public void InitializeSytem()
+        {
+
+            var dir = "";  Directory.GetCurrentDirectory();
+            //var Output = "";
+            var jpgCount = 0;
+            var rawCount = 0;
+
+            //Clear values incase this is the second run through from changing RAW file type combo box.
+            Output = "";
+            jpegsToRemoveCount = 0;
+            jpgsToRemove.Clear();
+
+            //Get Current working directory
+            dir = Directory.GetCurrentDirectory();
+
+            var RAWFile = RAWPicTypeTextComboBox.SelectedValue;
+
+            AddToOutput("Currently working in " + dir, true);
+
+            //Get count of RAW files.
+            rawCount = Directory.GetFiles(dir, "*." + RAWFile).Count();
+
+            //Get list of all JPG files
+            string[] fileEntries = Directory.GetFiles(dir, "*.jpg");
+
+            //Get number of JPGs found
+            jpgCount = fileEntries.Count();
+
+            AddToOutput("Found " + fileEntries.Count() + " JPG Files in the directory");
+
+            //Loop over all JPGs found in current running directory
+            foreach (string fileName in fileEntries)
+            {
+                // Test if the jpg has the a raw file with it. 
+                //Remove the end JPG file extension and add on RAW
+                var fileNameRAW = fileName.Substring(0, fileName.Length - 3) + RAWFile;
+
+                //If there is a RAW File with the same file name as the JPG then its safee to remove the jpeg.
+                if (File.Exists(fileNameRAW))
+                    jpgsToRemove.Add(fileName);
+              
+                AddToOutput("Processed file: " + System.IO.Path.GetFileName(fileName));
+            }
+
+            //Set jpg to remove count for future use
+            jpegsToRemoveCount = jpgsToRemove.Count();
+
+            AddToOutput("");
+            AddToOutput("Found " + jpgCount + " JPG Files.");
+            AddToOutput("Found " + rawCount + " RAW " + RAWFile + " Files.");
+
+            AddToOutput("Found " + jpgsToRemove.Count() + " JPGs that have matching RAW " + RAWFile + " Files that can be removed.");
+
+        }
+
+        private void DeleteJPGs_Click(object sender, RoutedEventArgs e)
+        {
+            //Show Yes no Box
+            var result = MessageBox.Show("Do you want to delete all " + jpegsToRemoveCount + " JPG pictures from the working directory?", "Are you sure you want to delete?", MessageBoxButton.YesNo, MessageBoxImage.Question);
+
+            if (result == MessageBoxResult.Yes)
+            {
+
+                //Show Yes no Box
+                var resultDoubleCheck = MessageBox.Show("Just double checking do you really want to delete all of your JPGs?", "Just double checking", MessageBoxButton.YesNo, MessageBoxImage.Question);
+
+                if (result == MessageBoxResult.Yes)
+                {
+                    foreach (var jpgFile in jpgsToRemove)
+                        File.Delete(jpgFile);
+
+                    AddToOutput("Removed " + jpegsToRemoveCount + " JPG files from the directory.", true);
+                }              
+            }
+        }
+
+        private void RAWPicTypeTextComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (!IsLoaded)
+            {
+                return;
+            }
+
+            InitializeSytem();
+        }
+
+        private void AddToOutput(string input, Boolean DoubleNewLine = false)
+        {
+
+            Output += input;
+
+            if (!DoubleNewLine)
+                Output += Environment.NewLine;
+            else
+                Output += Environment.NewLine + Environment.NewLine;
+
+            MainOutputTextBox.Text = Output;
+        }
+
+        private void HelpAbout(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("About and help info goes in this box.", "Help / About", MessageBoxButton.OK);
+        }
+    }
+
+ 
+}
